1121a99038f0688fd4ab1aaf69d12a3b58c2462e
ag93999 <adityagupta93999@gmail.com>
2018-07-23 17:38:40 +0530

    Upgraded drfaddons from 0.0.2 to 0.0.3 and changed '<=' to '>=' in 'requirements.txt'.

--- drf_contact/views.py
+++ drf_contact/views.py
@@ -8,14 +8,14 @@ class ShowContacts(ListAPIView):
     from .serializers import ShowContactDetailSerializer
     from rest_framework.filters import SearchFilter
     from .models import ContactDetail
-    from drfaddons.serializer import IsOwnerFilterBackend
+    from drfaddons.filters import IsOwnerFilterBackend
     from django_filters.rest_framework import DjangoFilterBackend
     from rest_framework.pagination import PageNumberPagination
     from rest_framework.permissions import IsAuthenticated
     from rest_framework.renderers import JSONRenderer
     from rest_framework.parsers import JSONParser
 
-    queryset = ContactDetail.objects.all()
+    queryset = ContactDetail.objects.all().order_by('-create_date')
     serializer_class = ShowContactDetailSerializer
     filter_backends = (IsOwnerFilterBackend, DjangoFilterBackend, SearchFilter)
     search_fields = ('^name',)
